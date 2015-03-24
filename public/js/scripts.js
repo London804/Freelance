ad798ff993ab7f33237c8db8be3f2ff1e3781ac4
Alex <alexchaparro@Alexs-MacBook-Pro.local>
2015-03-24 17:22:33 -0400

    added custom theme for google maps

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,4 @@
 $(function(){
 
-   var hello = 'Hello World';
-   var newTextLast = document.createTextNode('cream');
-   var newLi = document.createElement('div');
-   var newNode = newLi.appendChild(newTextLast);
-   var append =
 
-   console.log(newTextLast);
 });
