87f80080feee8fbaafeb6106395b4d805fdea75f
Alex <alexchaparro@Alexs-MacBook-Pro.local>
2015-03-24 20:01:55 -0400

    figured out capatcha and escaping a character in ejs

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,16 @@ exports.portfolio = function(req, res){
     });
 };
 exports.contact = function(req, res){
-    res.render('contact', {
-        title: 'Contact',
-        classname: 'contact'
+    var sweetcaptcha = new require('sweetcaptcha')(235723, '5d0150912bd74d200ef71f6cc5bb7c7b', '307af2781e22575d1bd08b6d171084d3');
+    sweetcaptcha.api('get_html', function(err,html) {
+        res.render('contact', {
+            title: 'Contact',
+            classname: 'contact',
+            captcha: html
+        });
     });
+    //Require the sweetcaptcha module and give it the credentials you were sent upon registration.
+
+
+
 };
